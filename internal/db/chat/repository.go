6169c9ab4a6047d404fec860def5b06ff61df447
m.kosenko <m.kosenko@vk.team>
2025-10-05 15:30:39 +0500

    day 3: model clarifications

--- internal/db/chat/repository.go
+++ internal/db/chat/repository.go
@@ -4,7 +4,7 @@ import "context"
 
 type Repository interface {
 	Init() error
-	Close() error
+	CloseConnection() error
 	Upsert(ctx context.Context, chatID int64, tz string) error
 	GetById(ctx context.Context, chatID int64) (tz string, found bool, err error)
 	DeleteById(ctx context.Context, chatID int64) (bool, error)
--- internal/db/chat/sqlite/repository.go
+++ internal/db/chat/sqlite/repository.go
@@ -18,25 +18,25 @@ func NewRepositorySQlite(db *sql.DB) *RepositorySQlite {
 func (r *RepositorySQlite) Init() error {
 	_, err := r.db.Exec(createTable)
 	if err != nil {
-		log.Println("[RepositorySQlite.Init] failed to create table:", err)
+		log.Println("[chat/RepositorySQlite.Init] failed to create table:", err)
 		return err
 	}
-	log.Println("[RepositorySQlite.Init] table created or already exists")
+	log.Println("[chat/RepositorySQlite.Init] table created or already exists")
 	return nil
 }
 
-func (r *RepositorySQlite) Close() error {
-	log.Println("[RepositorySQlite.Close] closing db connection")
+func (r *RepositorySQlite) CloseConnection() error {
+	log.Println("[chat/RepositorySQlite.Close] closing db connection")
 	return r.db.Close()
 }
 
 func (r *RepositorySQlite) Upsert(ctx context.Context, chatID int64, tz string) error {
 	_, err := r.db.ExecContext(ctx, upsert, chatID, tz)
 	if err != nil {
-		log.Printf("[RepositorySQlite.Upsert] chatID=%d tz=%s error=%v", chatID, tz, err)
+		log.Printf("[chat/RepositorySQlite.Upsert] chatID=%d tz=%s error=%v", chatID, tz, err)
 		return err
 	}
-	log.Printf("[RepositorySQlite.Upsert] success chatID=%d tz=%s", chatID, tz)
+	log.Printf("[chat/RepositorySQlite.Upsert] success chatID=%d tz=%s", chatID, tz)
 	return nil
 }
 
@@ -44,13 +44,13 @@ func (r *RepositorySQlite) GetById(ctx context.Context, chatID int64) (tz string
 	row := r.db.QueryRowContext(ctx, selectByChatId, chatID)
 	switch err = row.Scan(&tz); {
 	case err == nil:
-		log.Printf("[RepositorySQlite.GetById] found chatID=%d tz=%s", chatID, tz)
+		log.Printf("[chat/RepositorySQlite.GetById] found chatID=%d tz=%s", chatID, tz)
 		return tz, true, nil
 	case errors.Is(err, sql.ErrNoRows):
-		log.Printf("[RepositorySQlite.GetById] not found chatID=%d", chatID)
+		log.Printf("[chat/RepositorySQlite.GetById] not found chatID=%d", chatID)
 		return "", false, nil
 	default:
-		log.Printf("[RepositorySQlite.GetById] error chatID=%d err=%v", chatID, err)
+		log.Printf("[chat/RepositorySQlite.GetById] error chatID=%d err=%v", chatID, err)
 		return "", false, err
 	}
 }
@@ -58,21 +58,21 @@ func (r *RepositorySQlite) GetById(ctx context.Context, chatID int64) (tz string
 func (r *RepositorySQlite) DeleteById(ctx context.Context, chatID int64) (bool, error) {
 	res, err := r.db.ExecContext(ctx, deleteByChatId, chatID)
 	if err != nil {
-		log.Printf("[RepositorySQlite.DeleteById] chatID=%d error=%v", chatID, err)
+		log.Printf("[chat/RepositorySQlite.DeleteById] chatID=%d error=%v", chatID, err)
 		return false, err
 	}
 
 	rows, err := res.RowsAffected()
 	if err != nil {
-		log.Printf("[RepositorySQlite.DeleteById] chatID=%d error getting RowsAffected=%v", chatID, err)
+		log.Printf("[chat/RepositorySQlite.DeleteById] chatID=%d error getting RowsAffected=%v", chatID, err)
 		return false, err
 	}
 
 	if rows > 0 {
-		log.Printf("[RepositorySQlite.DeleteById] success deleted chatID=%d", chatID)
+		log.Printf("[chat/RepositorySQlite.DeleteById] success deleted chatID=%d", chatID)
 		return true, nil
 	}
 
-	log.Printf("[RepositorySQlite.DeleteById] no record found chatID=%d", chatID)
+	log.Printf("[chat/RepositorySQlite.DeleteById] no record found chatID=%d", chatID)
 	return false, nil
 }
